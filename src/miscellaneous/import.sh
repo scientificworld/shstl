96ce2cabc0912a209f7706338c7919cac9931379
scientificworld <scientificworld@126.com>
2021-10-12 12:12:29 +0800

    there is a bug in the preprocessor, I'll fix it later.

--- src/miscellaneous/import.sh
+++ src/miscellaneous/import.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 #@ include alias.sh
-function @.miscellaneous.import() {
-	if [ ! -n "$1" ]; then
-		return 1
-	fi
+function @.miscellaneous.current_path() {
 	local _original_path="$(pwd)"
 	cd "$(dirname "$BASH_SOURCE")"
 	if [ -h "$BASH_SOURCE" ]; then
 		cd "$(dirname "$(readlink "$BASH_SOURCE")")"
 	fi
+	pwd
+	cd "$_original_path"
+}
+function @.miscellaneous.import() {
+	if [ ! -n "$1" ]; then
+		return 1
+	fi
+	cd "$(@.miscellaneous.current_path)"
 	if [ ! -f "$1" ]; then
 		return 1
 	fi
 	. "$1"
 	cd "$_original_path"
 }
-@.alias "@import" "@.miscellaneous.import"
+@alias "@import" "@.miscellaneous.import"
--- src/miscellaneous/import/main.sh
+++ src/miscellaneous/import/main.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#!/bin/bash
-function @.miscellaneous.alias() {
-	if [ -n "$2" ]; then
-		eval 'function '"$1"'(){ '"$2"' "$@"; }'
-	fi
-}
-function @.miscellaneous.import() {
-	if [ ! -n "$1" ]; then
-		return 1
-	fi
+
+function @.miscellaneous.current_path() {
 	local _original_path="$(pwd)"
 	cd "$(dirname "$BASH_SOURCE")"
 	if [ -h "$BASH_SOURCE" ]; then
 		cd "$(dirname "$(readlink "$BASH_SOURCE")")"
 	fi
+	pwd
+	cd "$_original_path"
+}
+function @.miscellaneous.import() {
+	if [ ! -n "$1" ]; then
+		return 1
+	fi
+	cd "$(@.miscellaneous.current_path)"
 	if [ ! -f "$1" ]; then
 		return 1
 	fi
 	. "$1"
 	cd "$_original_path"
 }
-@.miscellaneous.alias @import @.miscellaneous.import
+@alias "@import" "@.miscellaneous.import"
 @import success.sh
