f179849887ebb10c27c76294a832808a8bd3085d
scientificworld <scientificworld@126.com>
2021-10-14 08:54:55 +0800

    add platform/windows/script

---
+++ src/platform/windows/script/_main.sh
@@ -0,0 +1,2 @@
+#@ include ../script.sh
+@.platform.windows.script_vbs 'msgbox "hello"'
---
+++ src/platform/windows/script/main.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+function @.platform.windows.cscript() {
+	if [ ! -n "$1" ]; then
+		return 1
+	fi
+	cscript "$1"
+}
+function @.platform.windows.wscript() {
+	if [ ! -n "$1" ]; then
+		return 1
+	fi
+	wscript "$1"
+}
+function @.platform.windows.script_js() {
+	if [ ! -n "$1" ]; then
+		return 1
+	fi
+	local _temp_file="$(mktemp)"
+	echo "$1" > "$_temp_file"
+	cscript //e:jscript "$_temp_file"
+}
+function @.platform.windows.script_vbs() {
+	if [ ! -n "$1" ]; then
+		return 1
+	fi
+	local _temp_file="$(mktemp)"
+	echo "$1" > "$_temp_file"
+	cscript //e:vbscript "$_temp_file"
+}
+@.platform.windows.script_vbs 'msgbox "hello"'
